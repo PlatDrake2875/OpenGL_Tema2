8e2ccb72ecb62b4672ff8a2d96c8823bae775a39
Vlad <andriesvladandrei@gmail.com>
2024-01-17 08:45:38 +0200

    place models

--- OreoRun/OreoRun/LevelLoader.cpp
+++ OreoRun/OreoRun/LevelLoader.cpp
@@ -1,4 +1,6 @@
 #include "LevelLoader.h"
+#include <iostream>
+//#include <cstring>
 
 LevelLoader::LevelLoader()
 {
@@ -26,11 +28,31 @@ void LevelLoader::setModel(int index, Model* _model)
 	models[index] = _model;
 }
 
+void LevelLoader::translate_scale_rotate(std::string modalPath, int index, glm::vec3 dir, GLfloat scaleFactor, GLfloat deg, glm::vec3 rotDir)
+{
+	models.push_back(new Model(modalPath));
+	models[index]->translate(dir);
+	models[index]->scale(scaleFactor);
+	models[index]->rotate(deg, rotDir);
+}
+
 void LevelLoader::loadModels(Shader* shader)
 {
 	models.push_back(new Model("models/oreo/oreo_4.gltf")); // Oreo King
 	//models.push_back(new Model("models/christmas_ball/scene.gltf")); // Test christmas ball
 	models.push_back(new Model("skyboxes/skybox_1/scene.gltf", true)); // Oreo King
+
+	translate_scale_rotate("models/candy_cane/scene.gltf", 
+		2, 
+		glm::vec3(0.f, 100.f, 0.f), 
+		60.f, 
+		0, glm::vec3(1.0f, 0.f, 0.f));  // Candy cane
+	translate_scale_rotate("models/christmas_ball/scene.gltf", 3, glm::vec3(100.f, -200.f, 0.f), 20.f, 0, glm::vec3(1.f, 0.f, 0.f));  // Christmas ball
+	translate_scale_rotate("models/gingerbread_house/scene.gltf", 4, glm::vec3(150.f, 0.f, 0.f), 30.f, 175, glm::vec3(0.f, 1.f, 1.2f));  // Gingerbread house
+	translate_scale_rotate("models/gingerbread_house_1/scene.gltf", 5, glm::vec3(-300.f, 0.f, -100.f), 5.f, 175, glm::vec3(0.f, 1.f, 1.2f));  // Gingerbread house
+	translate_scale_rotate("models/milkbox/scene.gltf", 6, glm::vec3(100.f, 200.f, 0.f), 20.f, 0, glm::vec3(1.f, 0.f, 0.f));  // Milkbox
+	//translate_scale_rotate("models/oreo_cupcake/scene.gltf", 7, glm::vec3(-100.f, -200.f, 0.f), 0.1f, 20, glm::vec3(0.f, 0.f, 0.f));  // Oreocupcake
+
 
 	for (auto& model : models)
 		model->setShaderProgram(shader->ID);
